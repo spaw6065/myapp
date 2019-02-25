bfc0b16ffb895df883d3df10e2478b91c011b4bb
SumitPawar <sumitpawar@SumitPawars-MacBook-Air.local>
2019-02-25 22:11:54 +0530

    my first maven commit

---
+++ src/main/java/com/dev/testpkg/demodevclass.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package com.dev.testpkg;
+
+/**
+ * @author sumitpawar
+ *
+ */
+public class demodevclass {
+
+	private static int a =10;
+	private static int b=20;
+	
+	public static int sum(int inum1,int inum2) {
+		System.out.println("Entered sum function");
+		int isum = inum1+inum2;
+		System.out.println("Sum : "+isum);
+		return isum;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int sumval = 0;
+		System.out.println("Entered main function");
+		sumval = sum(a,b);
+		
+		System.out.println("Sum val is  : "+sumval);
+ 		
+	}
+
+}
---
+++ src/test/java/com/qa/testpkg/demotestclass.java
@@ -0,0 +1,51 @@
+/**
+ * 
+ */
+package com.qa.testpkg;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+/**
+ * @author sumitpawar
+ *
+ */
+public class demotestclass {
+
+	//static Logger logger = Logger.getLogger(demotestclass.class);
+			
+			
+	@Test
+	public void sum() {
+		int a =10;
+		int b=20;
+		System.out.println("Sum");
+		//logger.info("SUM");
+		
+		Assert.assertEquals(30, a+b);	
+	}
+	
+	@Test
+	public void divide() {
+		int a =10,b=20;
+		System.out.println("Divide");
+		//logger.info("DIVIDE");
+	Assert.assertEquals(2, b/a);	
+	}
+	
+	@Test
+	public void multiply() {
+		int a =10,b=20;
+		System.out.println("multiply");
+		//logger.info("MULTIPLY");
+		Assert.assertEquals(200, a*b);	
+	}
+	
+	@Test
+	public void subtract() {
+		int a =10,b=20;
+		//logger.info("SUBTRACT");
+		Assert.assertEquals(10, b-a);	
+	}
+}
